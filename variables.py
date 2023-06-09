acf5915458de24565e17d8d799cfd52f6823256a
angel2507 <ruizlozanoangel.cce@gmail.com>
2023-06-09 16:20:16 -0600

    02 variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+# Python es NO tipado
+
+name = "Angel"
+last_name = "ruiz"
+age = 18
+height = 1.68
+is_student = False
+groups = ["3A","3B","3C"]
+
+# function type
+print(type (name))
+print(type(age))
+print(type(height))
+print(type (is_student))
+print(type (groups))

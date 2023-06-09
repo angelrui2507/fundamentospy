346d37aaa6710be88e1b0ca8773e547cde303316
angel2507 <ruizlozanoangel.cce@gmail.com>
2023-06-09 16:58:02 -0600

    05 Operadores logicos y boleanos

---
+++ operadores_2.py
@@ -0,0 +1,18 @@
+# Operadores booleanos
+# == (igualdad)
+print(1 == 1 )
+# != (distinto)
+print( 1 != 1)
+# > (mayor que)
+print(1 > 1)
+# < (menor que)
+print(1 < 1)
+# Operadores Lógicos
+# and
+print(True and True)
+print(True and False)
+# or
+print(True or True)
+print(True or False)
+# not
+print(not True)

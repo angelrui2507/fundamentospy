90c1c87abb64d1f01645909b0a0dbb0e127563f3
angel2507 <ruizlozanoangel.cce@gmail.com>
2023-06-20 12:52:09 -0600

    10 ciclos

---
+++ ciclos.py
@@ -0,0 +1,42 @@
+# Ciclo While
+# While Expo_bool: True
+
+i = 0
+num = 7
+while i <= 10:
+    print(f" { num } * { i } = { num * i}")
+    i += 1
+else:
+    print("Ciclo termiando")
+# Ciclo infinito
+while True:
+     # Rompemos con break
+    break
+
+# El for recorre iterables
+# un iterable es algo que se puede recorrer
+
+# for variable in iterable:
+my_string = "Hola"
+for letra in my_string:
+    print(letra, end=", ")
+
+print()
+
+lista = ["uno", "dos", "tres", "cuatro"]
+for item in lista:
+    print(item)
+
+# function range
+# range(end)
+for i in range(10):
+    print (i, end=',')
+print()    
+# range(ini, end)
+for i in range(10, 20):
+    print(i, end=', ')
+print    
+# range(ini, end, step)  
+for i in range(20, 10, 2):
+    print(i, end= ', ')
+print()      
